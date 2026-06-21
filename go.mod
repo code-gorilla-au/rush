064e4585e1a012787d32d553a43bf4574a577a8d
frag223 <m.riquelme223@gmail.com>
2026-06-21 21:53:29 +1000

    wip generating AI service

--- go.mod
+++ go.mod
@@ -22,6 +22,7 @@ require (
 	github.com/clipperhouse/displaywidth v0.11.0 // indirect
 	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-faker/faker/v4 v4.8.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.4.0 // indirect
@@ -35,6 +36,7 @@ require (
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/sync v0.21.0 // indirect
 	golang.org/x/sys v0.46.0 // indirect
+	golang.org/x/text v0.38.0 // indirect
 	modernc.org/libc v1.72.3 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
 	modernc.org/memory v1.11.0 // indirect
---
+++ internal/tournament/teams.go
@@ -0,0 +1,76 @@
+package tournament
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/code-gorilla-au/rush/internal/playbooks"
+	"github.com/code-gorilla-au/rush/internal/teams"
+	"github.com/go-faker/faker/v4"
+)
+
+const totalTeams = 12
+
+type AITeam struct {
+	CoachName string `faker:"name"`
+	TeamName  string `faker:"username"`
+}
+
+type AITeamService struct {
+	teamsSvc     *teams.Service
+	playbooksSvc *playbooks.Service
+}
+
+func (s *AITeamService) GenerateTeams(ctx context.Context) error {
+	aiTeams, err := generateAITeams()
+	if err != nil {
+		return fmt.Errorf("generating AI teams: %w", err)
+	}
+
+	for _, team := range aiTeams {
+		if err = s.generateTeam(ctx, team); err != nil {
+			return fmt.Errorf("generating team: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (s *AITeamService) generateTeam(ctx context.Context, team AITeam) error {
+	coach, err := s.teamsSvc.CreateCoach(ctx, team.CoachName, false)
+	if err != nil {
+		return fmt.Errorf("creating AI coach: %w", err)
+	}
+
+	aiTeam, err := s.teamsSvc.CreateTeam(nil, team.TeamName, coach.ID, false)
+	if err != nil {
+		return fmt.Errorf("creating team: %w", err)
+	}
+
+	if _, err = s.playbooksSvc.CreatePlaybook(ctx, playbooks.PlaybookParams{
+		TeamID:      aiTeam.ID,
+		Name:        fmt.Sprintf("%s Playbook", aiTeam.Name),
+		Description: "AI generated playbook",
+		Formations:  []playbooks.Formation{},
+	}); err != nil {
+		return fmt.Errorf("creating AI playbook: %w", err)
+	}
+
+	return nil
+}
+
+func generateAITeams() ([]AITeam, error) {
+	aiTeams := make([]AITeam, totalTeams)
+
+	for i := 0; i < totalTeams; i++ {
+		tmpTeam := AITeam{}
+
+		if err := faker.FakeData(&tmpTeam); err != nil {
+			return nil, fmt.Errorf("generating team: %w", err)
+		}
+
+		aiTeams[i] = tmpTeam
+	}
+
+	return aiTeams, nil
+}
